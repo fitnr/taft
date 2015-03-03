48e3bc60ba057fa322e924ec084a812e2cc660f9
fitnr <fitnr@fakeisthenewreal>
2015-03-03 02:55:43 -0500

    fiat taft

---
+++ bin/taft.js
@@ -0,0 +1,113 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var path = require('path'),
+    program = require('commander'),
+    glob = require('glob'),
+    YAML = require('js-yaml'),
+    taft = require('..');
+
+program
+    .version('0.0.1')
+    .usage('[options] <file ...>')
+    .description('Render a file with Handlebars')
+    .option('-t, --template <file>', 'template file', String)
+    .option('-h, --helpers <file>', 'File with handlebars helpers', String)
+    .option('-p, --partials <file or pattern>', 'Partials', String)
+    .option('-d, --data <data>', 'JSON or YAML data.', String)
+    .option('-o, --output <path>', 'output path or directory (mandatory if more than one file given', String, '-')
+    .option('-e, --ext <string>', 'output file extension', String, 'html')
+    .parse(process.argv);
+
+function parseData(data, noStdin) {
+    var result;
+    if (!data)
+        result = {};
+
+    else if (data === '-' && !noStdin) {
+        var stdin;
+        console.log('hi')
+        process.stdin.setEncoding('utf8');
+
+        process.stdin.on('readable', function() {
+            var chunk = process.stdin.read();
+            if (chunk !== null) stdin += chunk;
+        });
+
+        process.stdin.on('end', function() {
+            // recurse!
+            result = parseData(stdin, true);
+        });
+    }
+    // read yaml
+    else if (data.slice(-3) === '---' || data.slice(-4).toLowerCase() === 'yaml') 
+        result = yaml.safeLoad(data);
+        
+    // read json
+    else if (data.slice(-4).toLowerCase() === 'json')
+        fs.readFile(data, function(err, contents){
+            if (err) console.stderr.write("Couldn't read data file");
+            result = JSON.parse(contents);
+        });
+
+    else if (data.slice(0, 1) == '{' && data.slice(-1) == '}')
+        result = JSON.parse(data);
+
+    return result;
+}
+
+function outFile(file, outpath, ext) {
+    return path.join(
+        outpath,
+        path.basename(file, path.extname(file)) + '.' + ext
+    );
+}
+
+// expand files
+var files = [];
+for (var i = 0, len = program.args.length; i < len; i++)
+    Array.prototype.push.apply(files, glob.sync(program.args[i]));
+
+// check arguments
+var err = '';
+
+if (files.length == 0)
+    err += 'error - please provide an input file\n';
+
+if (files.length > 1) {
+
+    if (!fs.lstatSync(program.output).isDirectory())
+        program.output = path.dirname(program.output);
+
+    if (!fs.lstatSync().isDirectory(program.output))
+        err += 'error - output directory not found\n';
+
+}
+if (err) {
+    process.stderr.write(err);
+    process.exit(1);
+}
+
+//setup options
+var data = parseData(program.data),
+    options = {
+        template: program.template || undefined,
+        partials: program.partials ? glob.sync(program.partials).found : undefined
+    };
+    
+if (program.helpers)
+    options.helpers = require(path.basename(program.helpers, path.extname(program.helpers)));
+
+// render output
+if (program.output === '-')
+    console.log(taft(files[0], data, options));
+
+else for (var i = 0, len = files.length, outFile, output; i < len; i++) {
+    outfile = outFile(files[i], program.output, process.ext);
+    output = taft(files[i], data, options);
+
+    fs.writeFile(output, outfile, function(err) {
+        if (err) process.stderr.write(err);
+    });
+}
---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var fs = require('fs'),
+    path = require('path'),
+    extend = require('extend'),
+    Handlebars = require('handlebars'),
+    YFM = require('yfm');
+
+var taft = function(file, data, options) {
+    if (typeof(options) === 'undefined') {
+        options = data;
+        data = {};
+    }
+
+    data = data || {};
+    var rawfile;
+
+    try {
+        rawfile = fs.readFileSync(file, {encoding: 'utf8'});
+    } catch (err) {
+        if (err.name == 'TypeError') rawfile = file;
+        else throw(err)
+    }
+
+    Handlebars.registerHelper(options.helpers || {});
+    registerPartials(options.partials || []);
+
+    var source = YFM(rawfile),
+        format = Handlebars.compile(source.content.trimLeft()),
+        content = format(extend(data, source.context));
+
+    if (options.template)
+        content = taft(options.template, {page: data}, {
+            partials: {
+                body: content
+            }
+        });
+
+    return content;
+}
+
+var registerPartials = function(partials) {
+    if (Array.isArray(partials))
+        for (var i = 0, len = partials.length, name; i < len; i++) {
+            name = path.basename(items[i], path.extname(items[i]));
+
+            try {
+                Handlebars.registerPartial(name, fs.readFileSync(items[i]));
+            } catch (err) {
+                console.error("Could not register partial: " + name);
+            }
+        }
+
+    else if (typeof(partials) === 'object')
+        for (var name in partials)
+            if (partials.hasOwnProperty(name))
+                Handlebars.registerPartial(name, partials[name]);
+};
+
+module.exports = taft;
