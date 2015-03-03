9b1d38eb43023d83f9def23598a53c899fcb3d33
fitnr <fitnr@fakeisthenewreal>
2015-03-03 10:12:33 -0500

    simplify reading stdin with concat

--- bin/taft.js
+++ bin/taft.js
@@ -3,6 +3,8 @@
 'use strict';
 
 var path = require('path'),
+    fs = require('fs'),
+    concat = require('concat'),
     program = require('commander'),
     glob = require('glob'),
     Taft = require('..').Taft;
@@ -24,20 +26,10 @@ function parseData(data, noStdin) {
     if (!data)
         result = {};
 
-    else if (data === '-' && !noStdin) {
-        var stdin;
-
-        process.stdin.setEncoding('utf8');
-        process.stdin.on('readable', function() {
-            var chunk = process.stdin.read();
-            if (chunk !== null) stdin += chunk;
-        });
-
-        process.stdin.on('end', function() {
-            // recurse!
-            result = parseData(stdin, true);
-        });
-    }
+    else if (data === '-' && !noStdin)
+        process.stdin.pipe(concat(function(data){
+            result = parseData(data, true);
+        }));
 
     // read yaml
     else if (data.slice(-3) === '---' || data.slice(-4).toLowerCase() === 'yaml') {
@@ -48,7 +40,7 @@ function parseData(data, noStdin) {
     // read json
     else if (data.slice(-4).toLowerCase() === 'json')
         fs.readFile(data, function(err, contents){
-            if (err) console.stderr.write("Couldn't read data file");
+            if (err) process.stderr.write("Couldn't read data file");
             result = JSON.parse(contents);
         });
 
@@ -67,15 +59,23 @@ function outFile(file, outpath, ext) {
 
 // expand files
 var files = [];
-for (var i = 0, len = program.args.length; i < len; i++)
-    Array.prototype.push.apply(files, glob.sync(program.args[i]));
+for (var i = 0, len = program.args.length; i < len; i++) {
+    if (program.args[i] === '-')
+        file.push('-');
+    else 
+        Array.prototype.push.apply(files, glob.sync(program.args[i], {nonull: true}));
+}
 
 // check arguments
-var err = '';
+var err = '', warn = '';
 if (files.length == 0)
     err += 'error - please provide an input file\n';
 
-if (files.length > 1) {
+if (files.indexOf('-') > -1) {
+    if (files.length > 1) {
+        warn += "warning - ignoring stdin because other files given";
+        files.splice(files.indexOf('-'), 1);
+    }
 
     if (!fs.lstatSync(program.output).isDirectory())
         program.output = path.dirname(program.output);
@@ -83,10 +83,10 @@ if (files.length > 1) {
     if (!fs.lstatSync().isDirectory(program.output))
         err += 'error - output directory not found\n';
 
-}
-if (err) {
-    process.stderr.write(err);
-    process.exit(1);
+if (err || warn) {
+    process.stderr.write(err || warn);
+    if (err)
+        process.exit(1);
 }
 
 //setup options
@@ -99,11 +99,20 @@ var data = parseData(program.data),
 if (program.helper)
     options.helpers = require(path.join(process.cwd(), program.helper));
 
+// read STDIN if necessary
+if (files.indexOf('-') > -1) {
+    var j = files.indexOf('-');
+    
+    process.stdin.pipe(concat(function(data){
+        files[j] = data;
+    }));
+}
+
 // render output
 var taft = new Taft(data, options);
 
 if (program.output === '-')
-    console.log(taft.eat(files[0]));
+    process.stdout.write(taft.eat(files[0]));
 
 else for (var i = 0, len = files.length, outFile, output; i < len; i++) {
     outfile = outFile(files[i], program.output, process.ext);
