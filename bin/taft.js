10d7bd64f0ca8549fea46125905fbdfe188c7d76
fitnr <fitnr@fakeisthenewreal>
2015-11-18 22:16:06 -0500

    v0.2.0

--- bin/taft.js
+++ bin/taft.js
@@ -18,7 +18,7 @@ function collect(val, memo) {
 }
 
 program
-    .version('0.1.3')
+    .version('0.2.0')
     .usage('[options] <file ...>')
     .description('Render files with Handlebars')
     .option('-H, --helper <file>', 'js file that exports an object containing handlebars helpers', collect, [])
